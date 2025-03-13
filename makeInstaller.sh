e3c46e13a36dcffbb03654a9a2a7516edef77790
Badinszky Daniel <badinszky97@gmail.com>
2025-03-13 19:52:28 +0100

    postinstall deleted

--- makeInstaller.sh
+++ makeInstaller.sh
@@ -12,51 +12,6 @@ echo -e "Architecture: amd64" >> /tmp/productManager/DEBIAN/control
 echo -e "Maintainer: Daniel Badinszky <badinszky97@gmail.com>" >> /tmp/productManager/DEBIAN/control
 echo -e "Description: An application for managing a BOM hierarchy of a small/medium company." >> /tmp/productManager/DEBIAN/control
 
-cat <<EOF >> /tmp/productManager/DEBIAN/postinst
-#!/bin/bash
-
-# MariaDB telepitese
-while true; do
-    # Prompt user for input
-    read -p "Install MariaDB? Enter Y for Yes or N for No: " user_input
-
-    # Validate the input
-    if [[ "$user_input" == "Y" || "$user_input" == "y" ]]; then
-        echo "You selected Yes."
-        break
-    elif [[ "$user_input" == "N" || "$user_input" == "n" ]]; then
-        echo "You selected No."
-        break
-    else
-        echo "Invalid input. Please enter Y or N."
-    fi
-done
-
-# Megfelelő válasz
-if [[ "$user_input" == "Y" || "$user_input" == "y" ]]; then
-    echo "Telepithető"
-    echo "MariaDB ROOT jelszo:"
-    read -r rootpass
-
-    sudo apt update && sudo apt upgrade
-    sudo apt -y install software-properties-common
-    sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-    sudo add-apt-repository -y 'deb [arch=amd64] http://mariadb.mirror.globo.tech/repo/10.5/ubuntu $(lsb_release -cs) main'
-    sudo apt update
-    sudo apt install mariadb-server-11-6-2 mariadb-client
-    sudo mysql_secure_installation --set-root-pass=$rootpass --remove-anonymous-user --disallow-root --remove-test-db
-
-
-
-    
-
-
-fi
-
-
-
-
-EOF
 
 chmod +x /tmp/productManager/opt/productManager/manage/manage
 chmod 775 /tmp/productManager/DEBIAN/postinst
